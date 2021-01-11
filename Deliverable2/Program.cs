230d166fb2cb10fa4757097e5676090001666e23
antonio <antoniomanzari@gmail.com>
2021-01-10 19:54:25 -0500

    Added simple FlipCoin random bool generator method

--- Deliverable2/Program.cs
+++ Deliverable2/Program.cs
@@ -6,7 +6,22 @@ namespace Deliverable2
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string headsOrTailsGuess = System.String.Empty;
+            int numberOfFlips;
+            int correctCount;
+            Random rnd = new Random();
+
+            while (true)
+            {
+                Console.WriteLine(FlipCoin());
+                Console.ReadKey();
+            }
+
+            bool FlipCoin()
+            {
+                return rnd.Next(2) == 0;
+            }
+
         }
     }
 }
