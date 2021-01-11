76db0fe157f7c66798f5c989e868fc54cc8b59e1
antonio <antoniomanzari@gmail.com>
2021-01-10 21:11:49 -0500

    Fully functional(?)

--- Deliverable2/Program.cs
+++ Deliverable2/Program.cs
@@ -7,30 +7,80 @@ namespace Deliverable2
         static void Main(string[] args)
         {
             string headsOrTailsGuess = System.String.Empty;
-            int numberOfFlips;
-            int correctCount;
+            int numberOfFlips = 0;
+            int correctCount = 0;
             Random rnd = new Random();
 
-            Console.Write("Guess which  will have more: heads or tails? ");
-            headsOrTailsGuess = Console.ReadLine();
-            Console.Write("How many times shall we flip a coin? ");
-            numberOfFlips = Convert.ToInt32(Console.ReadLine());
+            // Main Sequence
+            GetUserInput();
+            ProcessCoinFlips();
+            PrintPlayerScore();
 
-            //test
-            Console.WriteLine(headsOrTailsGuess + ", " + numberOfFlips);
-            
-
-
-/*            while (true)
+            #region Methods
+            void ProcessCoinFlips()
             {
-                Console.WriteLine(ConvertBoolToCoinWords(FlipCoin()));
-                Console.ReadKey(true);
-            }*/
+                bool flipResult;
 
-            // Methods
+                for (int i = 0; i < numberOfFlips; i++)
+                {
+                    flipResult = FlipCoin();
+
+                    Console.WriteLine(ConvertBoolToCoinWords(flipResult));
+
+                    if (ConvertCoinWordsToBool(headsOrTailsGuess) == flipResult)
+                    {
+                        correctCount++;
+                    }
+                }
+            }
+
+            void PrintPlayerScore()
+            {
+                string timesGrammar = "times";
+
+                if (numberOfFlips == 1)
+                {
+                    timesGrammar = "time";
+                }
+
+                Console.WriteLine($"Your guess, {headsOrTailsGuess}, came up {correctCount} {timesGrammar}.");
+                Console.WriteLine($"That's {ProcessScore()}%.");
+            }
+
+            double ProcessScore()
+            {
+                Console.WriteLine(correctCount);
+                Console.WriteLine(numberOfFlips);
+                return Math.Round((correctCount / (double)numberOfFlips) * 100);
+            }
+
+            void GetUserInput()
+            {
+                Console.Write("Guess which  will have more: heads or tails? ");
+                headsOrTailsGuess = Console.ReadLine();
+                Console.Write("How many times shall we flip a coin? ");
+                numberOfFlips = Convert.ToInt32(Console.ReadLine());
+            }
+
             bool FlipCoin()
             {
                 return rnd.Next(2) == 0;
+            }
+
+            bool ConvertCoinWordsToBool(string coinGuess)
+            {
+                bool result = false;
+
+                if (coinGuess == "heads")
+                {
+                    result = true;
+                }
+                else if (coinGuess == "tails")
+                {
+                    result = false;
+                }
+
+                return result;
             }
 
             string ConvertBoolToCoinWords(bool coinState)
@@ -48,6 +98,7 @@ namespace Deliverable2
 
                 return result;
             }
+            #endregion
 
         }
     }
