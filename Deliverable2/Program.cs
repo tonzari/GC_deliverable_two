916225bed40c22840983445ada3f73d42216bb5f
antonio <antoniomanzari@gmail.com>
2021-01-10 20:16:30 -0500

    Added input feature, bool to string conversion feature

--- Deliverable2/Program.cs
+++ Deliverable2/Program.cs
@@ -11,15 +11,42 @@ namespace Deliverable2
             int correctCount;
             Random rnd = new Random();
 
-            while (true)
-            {
-                Console.WriteLine(FlipCoin());
-                Console.ReadKey();
-            }
+            Console.Write("Guess which  will have more: heads or tails? ");
+            headsOrTailsGuess = Console.ReadLine();
+            Console.Write("How many times shall we flip a coin? ");
+            numberOfFlips = Convert.ToInt32(Console.ReadLine());
 
+            //test
+            Console.WriteLine(headsOrTailsGuess + ", " + numberOfFlips);
+            
+
+
+/*            while (true)
+            {
+                Console.WriteLine(ConvertBoolToCoinWords(FlipCoin()));
+                Console.ReadKey(true);
+            }*/
+
+            // Methods
             bool FlipCoin()
             {
                 return rnd.Next(2) == 0;
+            }
+
+            string ConvertBoolToCoinWords(bool coinState)
+            {
+                string result = System.String.Empty;
+
+                if (coinState)
+                {
+                    result = "heads";
+                }
+                else
+                {
+                    result = "tails";
+                }
+
+                return result;
             }
 
         }
